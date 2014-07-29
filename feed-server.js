d6849a1b380d0292a04df5f58900511adcd21c8a
chirag sanghvi <csanghvi@appacitive.com>
2014-07-29 18:08:30 +0530

    added google feed api wrapper

---
+++ feed-server.js
@@ -0,0 +1,58 @@
+// Load Http library
+var http = require('http'),
+	sys = require('sys'),
+    Router = require('node-simple-router'),
+    gfeed = require('./feed-api.js'),
+	url = require('url');
+
+// Create a new instance of router
+var router = Router({ logging: false });
+
+// Route for get connections
+router.get('/', function (req, res) {
+ 
+
+  // Create a domain for exception handling
+  var domain = require('domain').create();
+
+  domain.on('error', function(err) {
+  	try {
+	  	res.writeHead(500, {'Content-Type': 'application/json'});
+		res.end(JSON.stringify({message: "Server Error" }));
+	} catch(e) {}
+
+    sys.puts("Error "  + err.message + '\n' + err.stack);
+    domain.dispose();
+  });
+
+  domain.run(function() {
+
+	  var url_parts = url.parse(req.url, true);
+	  var query = url_parts.query;
+	  var feed = new gfeed.Feed(query.q);
+	  feed.setNumEntries(-1);
+
+	  feed.listItems(function(response) {
+	  	    if (response.responseData != null) {
+	  			res.writeHead(200, {'Content-Type': 'application/json'});
+	  			res.end(JSON.stringify(response));
+	  		} else {
+	  			res.writeHead(400, {'Content-Type': 'application/json'});
+	  			res.end(JSON.stringify(response));
+	  		}
+	  });
+  });
+  
+});
+
+// Start an HTTP Server for logs with router
+var httpServer = require('http').createServer(router);
+
+httpServer.on('error', function(e) {
+  sys.puts("\n\nHttp server error " + e.message + "\n Stack: " + e.stack + "\n\n");
+});
+
+// Start listening on http-port in config
+httpServer.listen(8094);
+
+console.log("HTTP Server running at port 8094\n");
